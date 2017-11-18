f24fe9abd030d12a59599952450cb7161a233137
Fabio Albertin <albertin@gmail.com>
2017-11-18 01:23:47 +0000

    Finished section 8

--- pages/post.js
+++ pages/post.js
@@ -1,9 +1,32 @@
 import Layout from '../components/Layout.js';
+import Markdown from 'react-markdown';
 
 const Content = (props) => (
   <div>
     <h1>{props.url.query.title}</h1>
-    <p>Blog post content</p>
+    <div className="markdown">
+      <Markdown source={`
+        ### This is a title
+        Yes. We can have a [link](/link).
+        And here's the content.
+        `} />
+    </div>
+    <style jsx global>{`
+      .markdown { font-family: 'Arial', sans-serif; }
+
+      .markdown a {
+        text-decoration: none;
+        color: blue;
+      }
+
+      .markdown a:hover { opacity: 0.6; }
+
+      .markdown h3 {
+        margin: 0;
+        padding: 0;
+        text-transform: uppercase;
+      }
+      `}</style>
   </div>
 );
 
